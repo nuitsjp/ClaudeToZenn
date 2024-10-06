fd02f572a24cc05d6e8113c039b549c7c51f0939
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-10-06 16:13:40 +0900

    ExchangeGitHubToken クラスのリファクタリング
    - コンストラクタが `IHttpClientFactory` と `ILoggerFactory` を受け取るように変更
    - `ILogger` インスタンスを `loggerFactory.CreateLogger<ExchangeGitHubToken>()` で初期化
    - `HttpClient` インスタンスを `httpClientFactory.CreateClient()` で初期化
    - `Run` メソッドで `code` が空の場合に `BadRequest` を返すロジックを追加
    - `clientId` と `clientSecret` を環境変数から取得するロジックを追加
    - `HttpClient` のインスタンスを直接作成せず、`_httpClient` を使用して GitHub の OAuth トークンを取得するように変更
    - GitHub からのレスポンス内容をクライアントに返すロジックを追加

--- host/ClaudeToZenn.Functions/ExchangeGitHubToken.cs
+++ host/ClaudeToZenn.Functions/ExchangeGitHubToken.cs
@@ -3,51 +3,45 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
-namespace ClaudeToZenn.Functions
+namespace ClaudeToZenn.Functions;
+
+public class ExchangeGitHubToken(
+    IHttpClientFactory httpClientFactory,
+    ILoggerFactory loggerFactory)
 {
-    public class ExchangeGitHubToken
+    private readonly ILogger _logger = loggerFactory.CreateLogger<ExchangeGitHubToken>();
+    private HttpClient _httpClient = httpClientFactory.CreateClient();
+
+    [Function("ExchangeGitHubToken")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
     {
-        private readonly ILogger _logger;
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        public ExchangeGitHubToken(ILoggerFactory loggerFactory)
+        var query = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
+        string code = query["code"];
+
+        if (string.IsNullOrEmpty(code))
         {
-            _logger = loggerFactory.CreateLogger<ExchangeGitHubToken>();
+            var badRequestResponse = req.CreateResponse(HttpStatusCode.BadRequest);
+            await badRequestResponse.WriteStringAsync("Please pass a code on the query string");
+            return badRequestResponse;
         }
 
-        [Function("ExchangeGitHubToken")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        var clientId = Environment.GetEnvironmentVariable("GitHubClientId");
+        var clientSecret = Environment.GetEnvironmentVariable("GitHubClientSecret");
+
+        var values = new Dictionary<string, string>
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            { "client_id", clientId },
+            { "client_secret", clientSecret },
+            { "code", code }
+        };
+        var content = new FormUrlEncodedContent(values);
+        var response = await _httpClient.PostAsync("https://github.com/login/oauth/access_token", content);
+        var responseString = await response.Content.ReadAsStringAsync();
 
-            var query = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
-            string code = query["code"];
-
-            if (string.IsNullOrEmpty(code))
-            {
-                var badRequestResponse = req.CreateResponse(HttpStatusCode.BadRequest);
-                await badRequestResponse.WriteStringAsync("Please pass a code on the query string");
-                return badRequestResponse;
-            }
-
-            var clientId = Environment.GetEnvironmentVariable("GitHubClientId");
-            var clientSecret = Environment.GetEnvironmentVariable("GitHubClientSecret");
-
-            using (var client = new HttpClient())
-            {
-                var values = new Dictionary<string, string>
-                {
-                    { "client_id", clientId },
-                    { "client_secret", clientSecret },
-                    { "code", code }
-                };
-                var content = new FormUrlEncodedContent(values);
-                var response = await client.PostAsync("https://github.com/login/oauth/access_token", content);
-                var responseString = await response.Content.ReadAsStringAsync();
-
-                var okResponse = req.CreateResponse(HttpStatusCode.OK);
-                await okResponse.WriteStringAsync(responseString);
-                return okResponse;
-            }
-        }
+        var okResponse = req.CreateResponse(HttpStatusCode.OK);
+        await okResponse.WriteStringAsync(responseString);
+        return okResponse;
     }
 }
